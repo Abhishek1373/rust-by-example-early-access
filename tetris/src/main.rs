5617d8073d6988304e1485557c0c9320a8817d30
Lawrence Veigas <lancev.veigas3@gmail.com>
2017-12-15 23:23:54 +0530

    Updated main.rs
    Updated with drawing

--- tetris/src/main.rs
+++ tetris/src/main.rs
@@ -2,9 +2,12 @@ mod subfolder;
 
 extern crate sdl2;
 
+use sdl2::rect::Rect;
+use sdl2::render::{Canvas, Texture, TextureCreator};
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+
 use std::time::Duration;
 use std::thread::sleep;
 
@@ -12,21 +15,35 @@ pub fn main(){
   let sdl_context = sdl2::init().expect("SDL initialization failed");
   let video_subsystem = sdl_context.video().expect("Couldn't get SDL video subsytem");
 
-  let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
+  let window = video_subsystem.window("Tetris", 800, 600)
     .position_centered()
     .opengl()
     .build()
     .expect("Failed to create window");
 
-  let mut canvas = window.into_canvas().build().expect("Failed to convert window into canvas");
+  let mut canvas = window.into_canvas()
+    .target_texture()
+    .present_vsync()
+    .build()
+    .expect("Failed to convert window into canvas");
 
-  canvas.set_draw_color(Color::RGB(255, 0, 0));
-  canvas.clear();
-  canvas.present();
+  let texture_creator: TextureCreator<_> = canvas.texture_creator();
+
+  const TEXTURE_SIZE: u32 = 32;
+
+  let mut square_texture: Texture =
+      texture_creator.create_texture_target(None, TEXTURE_SIZE,
+          TEXTURE_SIZE)
+      .expect("Failed to create a texture");
+
+  canvas.with_texture_canvas(&mut square_texture, |texture| {
+      texture.set_draw_color(Color::RGB(0, 255, 0));
+      texture.clear();
+  }).expect("Failed to color a texture");
 
   let mut event_pump = sdl_context.event_pump().expect("Failed to get SDL event pump");
 
-  'running: loop {
+ 'running: loop {
       for event in event_pump.poll_iter(){
           match event {
               Event::Quit { .. } |
@@ -37,6 +54,16 @@ pub fn main(){
               _=> {}
           }
       }
-      sleep(Duration::new(0, 1_000_000_000u32 / 60));
+  canvas.set_draw_color(Color::RGB(255, 0, 0));
+  canvas.clear();
+
+  canvas.copy(&square_texture,
+                None,
+                Rect::new(0,0,TEXTURE_SIZE, TEXTURE_SIZE))
+        .expect("Couldn't copy texture into window");
+
+  canvas.present();
+
+  sleep(Duration::new(0, 1_000_000_000u32 / 60));
   }
 }
