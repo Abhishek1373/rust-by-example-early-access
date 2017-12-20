794fa2c0051d0094db395362065143d97023077d
Lawrence Veigas <lancev.veigas3@gmail.com>
2017-12-20 11:34:35 +0530

    Updated main.rs to read from file

--- chapter 2/handling_files/src/main.rs
+++ chapter 2/handling_files/src/main.rs
@@ -1,12 +1,20 @@
 use std::fs::File;
-use std::io::{self, Write};
+use std::io::{self, Write, Read};
 
 fn write_into_file(content: &str, file_name: &str) -> io::Result<()> {
     let mut f = File::create(file_name)?;
     f.write_all(content.as_bytes())
 }
 
+fn read_from_file(file_name: &str) -> io::Result<String> {
+    let mut f = File::open(file_name)?;
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+    Ok(content)
+}
+
 fn main(){
     write_into_file("Packt", "write_example");
 
+    println!("{:?}",read_from_file("read_example"));
 }
